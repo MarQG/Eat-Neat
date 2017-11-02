0fab4f23b17c105f33a9e523090859f05efbc80c
Peter Le <Peter Le@LC-PETERLAPTOP.lec.corp.com>
2017-11-02 14:50:22 -0500

    Working on Via JS Library to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,31 @@
 */
 $("document").ready(function(){
 	console.log("App.js linked");
+
+	var views = {
+		home: [{
+			selector: "#title",
+			templateUrl: '../views/home-title.html'
+		}, {
+			selector: "#content",
+			templateUrl: '../views/home-content.html'
+		},
+		],
+		search: [{
+			selector: "#title",
+			templateUrl: '../views/search-title.html'
+		}, {
+			selector: "#content",
+			templateUrl: '../views/search-content.html'
+		},
+		],
+
+		defaultView: {
+			view: 'home'
+		}
+};
+
+var via = new Via();
+
+via.init(views);
 });
